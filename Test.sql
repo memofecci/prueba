c79731addf2b39581904513ac24620acfbaad884
Guillermo <Guillermo@memofecci>
2016-05-06 14:53:43 -0300

    Completo 1.0

--- Test.sql
+++ Test.sql
@@ -1,2 +1,12 @@
-create databse Prueba;
+create database Prueba;
+use prueba;
+
+create table Usuarios(
+usuario_id int(10) primary key not null auto_increment,
+nombre varchar(25),
+apepat varchar(25),
+apemat varchar(25),
+usuario varchar(25),
+password varchar(250),
+perfil varchar(25));
 
